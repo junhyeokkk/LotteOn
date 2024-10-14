e6ad0fc8fcf65431219c628874c7aa8c56f3eacf
SeoHong23 <ghd2408@naver.com>
2024-10-14 14:18:29 +0900

    feat: 회사소개

---
+++ src/main/java/com/team1/lotteon/controller/company/PageController.java
@@ -0,0 +1,31 @@
+package com.team1.lotteon.controller.company;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Log4j2
+@Controller
+public class PageController {
+
+    @GetMapping("/company/index")
+    public String index(){
+
+        return "company/index";
+    }
+
+    @GetMapping("/company/layout/{group}/{cate}")
+    public String index(@PathVariable String group, @PathVariable String cate, Model model){
+
+        log.info("컨트롤러 들어오니?");
+        log.info("ggggggggg" + group);
+        log.info(cate);
+        model.addAttribute("group", group);
+        model.addAttribute("cate", cate);
+
+
+        return "company/layout/company_layout";
+    }
+}
