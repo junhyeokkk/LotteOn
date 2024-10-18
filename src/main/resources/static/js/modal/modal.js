c00ed268937a1d27d8da11a3e6a03b8d59161fd3
DoyoungLee9720 <dlehdud224@gmail.com>
2024-10-18 12:47:03 +0900

    feat : myinfo -> controller , modal 수정2

--- src/main/resources/static/js/modal/modal.js
+++ src/main/resources/static/js/modal/modal.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const modal = document.getElementById(modalId);
         if (modal) {
             modal.style.display = 'block';
+            document.body.style.overflow = 'hidden';  // 외부 스크롤 막기
         }
     }
 
@@ -13,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const modal = document.getElementById(modalId);
         if (modal) {
             modal.style.display = 'none';
+            document.body.style.overflow = '';  // 외부 스크롤 다시 활성화
         }
     }
 
@@ -20,6 +22,9 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelectorAll('[data-modal]').forEach(function (button) {
         button.addEventListener('click', function () {
             const modalId = this.getAttribute('data-modal');
+            if(modalId === "inquiryModal"){
+                closeModal('sellerInfoModal');
+            }
             console.log(modalId);
             openModal(modalId);
         });
@@ -30,6 +35,16 @@ document.addEventListener('DOMContentLoaded', function() {
         button.addEventListener('click', function () {
             const modal = button.closest('.modal');  // 가장 가까운 모달 요소를 찾음
             closeModal(modal.id);
+        });
+    });
+    // 별 클릭 이벤트
+    const modalstars = document.querySelectorAll('.modalstar');
+    modalstars.forEach((modalstar, index) => {
+        modalstar.addEventListener('click', () => {
+            modalstars.forEach(s => s.classList.remove('filled'));
+            for (let i = 0; i <= index; i++) {
+                modalstars[i].classList.add('filled');
+            }
         });
     });
 });
