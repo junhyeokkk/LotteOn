b2444079ec68c066a917a74e00799dbba929a2d5
sanghoon222 <sanghoon8046@gmail.com>
2024-10-22 10:02:34 +0900

    cicd 테스트

--- src/test/java/com/team1/lotteon/controller/admin/AdminControllerTest.java
+++
@@ -1,9 +0,0 @@
-package com.team1.lotteon.controller.admin;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import static org.junit.jupiter.api.Assertions.*;
-class AdminControllerTest {
-
-}
