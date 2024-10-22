d98d8063519b5d8f66a6406d4ce3ed6f30785382
sanghoon222 <sanghoon8046@gmail.com>
2024-10-22 15:49:09 +0900

    feat: 관리자 category view

--- src/main/java/com/team1/lotteon/controller/admin/config/ConfigPageController.java
+++ src/main/java/com/team1/lotteon/controller/admin/config/ConfigPageController.java
@@ -1,6 +1,8 @@
 package com.team1.lotteon.controller.admin.config;
 
 import com.team1.lotteon.dto.ConfigDTO;
+import com.team1.lotteon.dto.category.CategoryWithChildrenResponseDTO;
+import com.team1.lotteon.service.CategoryService;
 import com.team1.lotteon.service.admin.InfoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -8,13 +10,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Log4j2
 @RequiredArgsConstructor
 @Controller
 public class ConfigPageController {
-
+    private final CategoryService categoryService;
     private final InfoService infoService;
 
     // info 페이지 이동시 config 정보 담기
@@ -46,8 +51,10 @@ public class ConfigPageController {
     }
 
     @GetMapping("/admin/config/category")
-    public String category(){
-
+    public String category(Model model){
+        List<CategoryWithChildrenResponseDTO> allRootCategories = categoryService.getAllRootCategories();
+        model.addAttribute("categories", allRootCategories);
+        System.out.println("allRootCategories = " + allRootCategories);
         return "admin/config/category";
     }
 }
