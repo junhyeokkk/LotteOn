8182f89da9ab0b001d2090752ace0768cadd47a2
sanghoon222 <sanghoon8046@gmail.com>
2024-10-30 15:06:22 +0900

    feat: header-nav에 카테고리 출력 완료

--- src/main/java/com/team1/lotteon/controller/MainController.java
+++ src/main/java/com/team1/lotteon/controller/MainController.java
@@ -1,12 +1,23 @@
 package com.team1.lotteon.controller;
 
+import com.team1.lotteon.dto.category.CategoryWithChildrenResponseDTO;
+import com.team1.lotteon.service.CategoryService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+    private final CategoryService categoryService;
+
     @GetMapping
-    public String main() {
+    public String main(Model model) {
+        List<CategoryWithChildrenResponseDTO> allCategories = categoryService.getAllRootCategories();
+        model.addAttribute("categories", allCategories);
         return "main/layout/main_layout";
     }
 }
