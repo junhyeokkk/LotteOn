f652b87f96f13d8ff6ee0106f64f66039c304f9c
DoyoungLee9720 <dlehdud224@gmail.com>
2024-10-31 17:13:42 +0900

    coupontake 기능작업

--- src/main/java/com/team1/lotteon/dto/CouponTakeDTO.java
+++ src/main/java/com/team1/lotteon/dto/CouponTakeDTO.java
@@ -3,6 +3,9 @@ package com.team1.lotteon.dto;
      날짜 : 2024/10/30
      이름 : 이도영(최초 작성자)
      내용 : CouponTakeDTO 생성
+     
+     수정내용
+        - 2024-10-31 이도영 shopid 추가
 */
 import lombok.*;
 
@@ -17,6 +20,7 @@ public class CouponTakeDTO {
     private Long couponTakenId;
     private Long couponId;
     private String memberId;
+    private Long shopId;
     private LocalDateTime couponGetDate;
     private LocalDateTime couponExpireDate;
     private LocalDateTime couponUseDate;
